a8ae90a1aab5262476d65a91954df129884627b2
williambl <william@williambl.com>
2017-11-25 18:26:23 +0000

    Made a second activity

--- app/src/main/java/com/williambl/testapplication/Main.kt
+++ app/src/main/java/com/williambl/testapplication/Main.kt
@@ -4,14 +4,14 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import android.view.View
-
-
-
-
-
-
+import android.content.Intent
+import android.widget.EditText
 
 class Main : AppCompatActivity() {
+
+    companion object {
+        const val extraMessage = "com.williambl.testapplication.MESSAGE"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +25,14 @@ class Main : AppCompatActivity() {
 
         val toast = Toast.makeText(context, text, duration)
         toast.show();
+    }
+
+    fun sendMessage(view: View) {
+        val intent = Intent(this, DisplayMessageActivity::class.java)
+        val editText = findViewById<View>(R.id.editText) as EditText
+        val message = editText.text.toString()
+        intent.putExtra(extraMessage, message)
+        startActivity(intent)
     }
 
 }
